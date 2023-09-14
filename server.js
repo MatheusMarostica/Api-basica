d46c825905bd0ca2d839d5307f68f65aca7508a1
Matheus <matheus.marostica.lemos@gmail.com>
2023-09-14 09:55:55 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const api = express();
+
+api.get('/api/teste', (request, response) => {
+    response.send('teste')
+});
+api.listen(3001, () => console.log('show de bola'));
